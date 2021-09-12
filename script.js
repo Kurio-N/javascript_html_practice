909c5f0bba0ed3ba09eabc7853b2945958a66c31
Kurio-N <t.nk.gmail.com>
2021-09-12 11:37:14 +0900

    updated

--- script.js
+++ script.js
@@ -7,22 +7,31 @@ function show(elem) {
     result.value = result.value + elem.value;
 }
 function increase(id) {
-    var trg = document.getElementById(id)
+    var trg = document.getElementById(id);
     // trg.value = new Function("return 1+" + trg.value)();
     trg.value = parseInt(trg.value) + 1;
 }
 function decrease(id) {
-    var trg = document.getElementById(id)
+    var trg = document.getElementById(id);
     trg.value = new Function("return " + trg.value + "-1")();
 }
 function increase2(id) {
-    var trg = document.getElementById(id)
+    var trg = document.getElementById(id);
     // trg.value = new Function("return 1+" + trg.value)();
     trg.innerHTML = parseInt(trg.innerHTML) + 1;
 }
 function decrease2(id) {
-    var trg = document.getElementById(id)
-    trg.innerHTML = parseInt(trg.innerHTML) - 1;}
+    var trg = document.getElementById(id);
+    trg.innerHTML = parseInt(trg.innerHTML) - 1;
+}
+
+function apply(id1, id2){
+    var trg1 = document.getElementById(id1);
+    var trg2 = document.getElementById(id2);
+
+    trg2.innerHTML = trg1.innerHTML;
+}
+
 
 
 function calc_score() {
