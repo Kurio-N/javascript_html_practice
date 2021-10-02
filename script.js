3314afa0b70cad33332e716d470c012af1c71507
Kurio-N <t.nk.gmail.com>
2021-10-03 02:39:54 +0900

    updated

--- script.js
+++ script.js
@@ -49,33 +49,87 @@ function apply(id1, id2){
 // ===== jQueryに依存した関数 =====
 function add_player(){
 
-    // プレイヤー人数を増加
-    player_num += 1;
+    if (player_num <= MAX_PLAYER){
 
-    // プレイヤーごとのをコピー
-    var tmp = $("#player1").clone();
+        // プレイヤー人数を増加
+        player_num += 1;
 
-    // 表示名変更
-    tmp.find(".player-num").text("プレイヤー" + String(player_num));
+        // プレイヤー数が最大値になったら追加ボタンをdisable
+        if(player_num == MAX_PLAYER){
+            document.getElementById("player-add-btn").disabled = true;
+        }
+    
+        // プレイヤー数が2以上になったらプレイヤー削除ボタンをenable
+        if(player_num > 1){
+            document.getElementById("player1-sub-btn").disabled = false;
+        }
 
-    // IDを変更
-    tmp.attr('id', 'player' + String(player_num));
-    txt = tmp.html();
-    tmp.html(txt.replace(/player1/g, 'player'+String(player_num)));
+        // プレイヤーごとのをコピー
+        var tmp = $("#player1").clone();
 
-    // 表示
-    tmp.appendTo("#extra-players").hide().slideDown();
+        // 表示名変更
+        tmp.find(".player-num").text("プレイヤー" + String(player_num));
 
-    if(player_num == MAX_PLAYER){
-       document.getElementById("player-add-btn").disabled = true;
+        // IDを変更
+        tmp.attr('id', 'player' + String(player_num));
+        txt = tmp.html();
+        tmp.html(txt.replace(/player1/g, 'player'+String(player_num)));
+
+        // 表示
+        tmp.appendTo("#extra-players").hide().slideDown();
     }
 
 }
 
-function sub_player(){
-    $("#player2").slideUp();
-    $("#player2").remove();
-    player_num -= 1;
+function change_player_id(id_num_from, id_num_to){
+
+    var player_from = $("#player" + String(id_num_from))
+
+    // 表示名書き換え
+    player_from.find(".player-num").text("プレイヤー" + String(id_num_to))
+
+    // IDをすべて書き換え
+    player_from.attr('id', 'player' + String(id_num_to));
+    console.log(player_from.attr("id"))
+    txt = player_from.html();
+    player_from.html(txt.replace(/player\d/g, 'player'+String(id_num_to)));
+
+}
+
+function sub_player(player_id){
+
+    // プレイヤーが1人だけの場合は何も起きない
+    if(player_num > 1){
+
+
+        // そのプレイヤーを削除
+        $("#" + player_id).slideUp("normal", function(){
+            $(this).remove();
+        });
+
+
+        // player_id から数字だけ取得
+        id_num = parseInt(player_id.replace("player", ""))
+
+        // そのプレイヤーより後ろのプレイヤーを１つずつ下にスライド
+        for (let i = id_num; i < player_num; i++){
+            change_player_id(i+1, i)
+        }
+
+        // プレイヤー数を減少
+        player_num -= 1;
+        // プレイヤー数が最大値でなくなったらプレイヤー追加ボタンをenable
+        if(player_num < MAX_PLAYER){
+            document.getElementById("player-add-btn").disabled = false;
+         }
+
+        // プレイヤー数が一人になったら削除ボタンをdisabledに
+        if (player_num == 1){
+            document.getElementById("player1-sub-btn").disabled = true;
+        }
+
+    }
+
 }
 
 function calc_score() {
